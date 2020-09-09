09fefe758f5eddeeb85adf1665d082bcfd30a2d5
unknown <kacem.amine1112@gmail.com>
2020-09-09 11:46:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Counter : {props.count}
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => props.increment()}
+          >
+            {" "}
+            INCREMENT
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => props.count > 0 && props.decrement()}
+          >
+            DECREMENT
+          </Button>
+        </div>
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStatetoProps = (state) => ({
+  count: state.counter,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "INCREMENT" }),
+    decrement: () => dispatch({ type: "DECREMENT" }),
+  };
+};
+
+export default connect(mapStatetoProps, mapDispatchToProps)(App);
